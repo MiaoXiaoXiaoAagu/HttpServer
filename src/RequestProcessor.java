aec499541b59836a7411dfce433d8b03fce5c38a
yxy <15355462400@163.com>
2019-02-13 15:06:36 +0800

    add response method Head

--- src/RequestProcessor.java
+++ src/RequestProcessor.java
@@ -14,7 +14,7 @@ public class RequestProcessor implements Runnable {
   private Socket connection;
   
   public RequestProcessor(File rootDirectory, 
-      String indexFileName, Socket connection) {
+      String indexFileName, Socket connection) {//init for response method like post\get\put\head
         
     if (rootDirectory.isFile()) {
       throw new IllegalArgumentException(
@@ -57,60 +57,12 @@ public class RequestProcessor implements Runnable {
       
       String[] tokens = get.split("\\s+");
       String method = tokens[0];
-      String version = "";
-      if (method.equals("GET")) {
-        String fileName = tokens[1];
-        if (fileName.endsWith("/")) fileName += indexFileName;
-        String contentType = 
-            URLConnection.getFileNameMap().getContentTypeFor(fileName);
-        if (tokens.length > 2) {
-          version = tokens[2];
-        }
-
-        File theFile = new File(rootDirectory, 
-            fileName.substring(1, fileName.length()));
-        
-        if (theFile.canRead() 
-            // Don't let clients outside the document root
-            && theFile.getCanonicalPath().startsWith(root)) {
-          byte[] theData = Files.readAllBytes(theFile.toPath());
-          if (version.startsWith("HTTP/")) { // send a MIME header
-            sendHeader(out, "HTTP/1.0 200 OK", contentType, theData.length);
-          } 
-      
-          // send the file; it may be an image or other binary data 
-          // so use the underlying output stream 
-          // instead of the writer
-          raw.write(theData);
-          raw.flush();
-        } else { // can't find the file
-          String body = new StringBuilder("<HTML>\r\n")
-              .append("<HEAD><TITLE>File Not Found</TITLE>\r\n")
-              .append("</HEAD>\r\n")
-              .append("<BODY>")
-              .append("<H1>HTTP Error 404: File Not Found</H1>\r\n")
-              .append("</BODY></HTML>\r\n").toString();
-          if (version.startsWith("HTTP/")) { // send a MIME header
-            sendHeader(out, "HTTP/1.0 404 File Not Found", 
-                "text/html; charset=utf-8", body.length());
-          } 
-          out.write(body);
-          out.flush();
-        }
-      } else { // method does not equal "GET"
-        String body = new StringBuilder("<HTML>\r\n")
-            .append("<HEAD><TITLE>Not Implemented</TITLE>\r\n")
-            .append("</HEAD>\r\n")
-            .append("<BODY>")
-            .append("<H1>HTTP Error 501: Not Implemented</H1>\r\n")
-            .append("</BODY></HTML>\r\n").toString();
-        if (version.startsWith("HTTP/")) { // send a MIME header
-          sendHeader(out, "HTTP/1.0 501 Not Implemented", 
-                    "text/html; charset=utf-8", body.length());
-        }
-        out.write(body);
-        out.flush();
+      if (method.equals("GET")) {responseGet(tokens,out,raw,root);}
+      else if(method.equals("HEAD"))
+      {
+    	  responseHead(tokens,out,raw,root);
       }
+      
     } catch (IOException ex) {
       logger.log(Level.WARNING, 
           "Error talking to " + connection.getRemoteSocketAddress(), ex);
@@ -121,7 +73,65 @@ public class RequestProcessor implements Runnable {
       catch (IOException ex) {} 
     }
   }
+  private void responseHead(String[] requestLine,Writer out,OutputStream raw,String root) throws IOException
+  {
+	  String version = "";
+	  version = requestLine[2];
+	  String fileName = requestLine[1];
+	  String contentType = 
+	          URLConnection.getFileNameMap().getContentTypeFor(fileName);
+	  File theFile = new File(rootDirectory, 
+	          fileName.substring(1, fileName.length()));
+	      
+	      if (theFile.canRead() && theFile.getCanonicalPath().startsWith(root)) { // Don't let clients outside the document root
+		        if (version.startsWith("HTTP/")) { // send a MIME header
+		          sendHeader(out, "HTTP/1.1 204 OK", contentType, (int)theFile.length());
+		        } 
+	
+	      }
+  }
+  private void responseGet( String[] requestLine,Writer out,OutputStream raw,String root) throws IOException {
+	  String version = "";
+	  String fileName = requestLine[1];
+      if (fileName.endsWith("/")) fileName += indexFileName;
+      String contentType = 
+          URLConnection.getFileNameMap().getContentTypeFor(fileName);
+      if (requestLine.length > 2) {
+        version = requestLine[2];
+        
+      }
 
+      File theFile = new File(rootDirectory, 
+          fileName.substring(1, fileName.length()));
+      
+      if (theFile.canRead() 
+          // Don't let clients outside the document root
+          && theFile.getCanonicalPath().startsWith(root)) {
+        byte[] theData = Files.readAllBytes(theFile.toPath());
+        if (version.startsWith("HTTP/")) { // send a MIME header
+          sendHeader(out, "HTTP/1.1 200 OK", contentType, theData.length);
+        } 
+    
+        // send the file; it may be an image or other binary data 
+        // so use the underlying output stream 
+        // instead of the writer
+        raw.write(theData);
+        raw.flush();
+      } else { // can't find the file
+        String body = new StringBuilder("<HTML>\r\n")
+            .append("<HEAD><TITLE>File Not Found</TITLE>\r\n")
+            .append("</HEAD>\r\n")
+            .append("<BODY>")
+            .append("<H1>HTTP Error 404: File Not Found</H1>\r\n")
+            .append("</BODY></HTML>\r\n").toString();
+        if (version.startsWith("HTTP/")) { // send a MIME header
+          sendHeader(out, "HTTP/1.1 404 File Not Found", 
+              "text/html; charset=utf-8", body.length());
+        } 
+        out.write(body);
+        out.flush();
+      }
+  }
   private void sendHeader(Writer out, String responseCode,
       String contentType, int length)
       throws IOException {
